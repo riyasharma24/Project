d83f2480f3e5c42bbb0e9de4c6b9123fcd7bf62c
riya <riya@192.168.0.48>
2019-07-10 20:50:14 +0200

    commit

---
+++ FirstProject/src/first/demoinheritance.java
@@ -0,0 +1,61 @@
+package first;
+
+class calc
+{
+	public int add(int i,int j) {
+		return i+j;
+	}
+}
+class caladv extends calc
+{
+	public int sub(int i, int j) {
+		return i-j;
+	}
+}
+class calveryadv extends caladv
+{
+	public int multiplication(int i,int j) {
+		return i*j;
+	}
+}
+public class demoinheritance{
+	public static void main(String[] args) {
+		calveryadv c1 = new calveryadv();
+		int result1 = c1.add(3,5);
+		int result2 = c1.sub(9, 8);
+		int result3 = c1.multiplication(5, 9);
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+	}
+
+	
+		
+		
+	}
+
+
+ 
+
+
+
+
+ 
+ 
+
+
+
+
+
+	
+			
+		
+		
+		
+	
+	
+
+
+	
+
+	
