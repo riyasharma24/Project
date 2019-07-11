26fa6e357ef75b70e14283f423bc18f0697dff7c
riya <riya@192.168.0.48>
2019-07-11 10:43:42 +0200

    Forth commit

---
+++ FirstProject/src/first/Arraylist.java
@@ -0,0 +1,13 @@
+package first;
+import java.util.ArrayList;
+public class Arraylist {
+
+	public static void main(String[] args) {
+		ArrayList<String>cars=new ArrayList<String>(); 
+		cars.add("ford");
+		cars.add("BMW");
+		cars.add("audi");
+		
+		System.out.println(cars);
+	}
+}
